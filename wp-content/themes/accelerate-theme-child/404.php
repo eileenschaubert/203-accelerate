950a6ce86716393f17a6967d17b24632a1dafc94
Eileen Schaubert <eileen.schaubert@gmail.com>
2018-09-26 16:44:39 -0500

    add 404 page, styling to about page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying 404 page
+ *
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+	<div class="site-content four-oh-four">
+		<div class="main-content">
+			<h2>Whoops, Took a Wrong Turn...</h2>
+			<p>Sorry, this page no longer exists, never existed or has been moved. We feel like complete jerks for misleading you.</p>
+			<p>Feel free to take a look around our <a href="blog">blog</a> or our featured <a href="case-studies">work</a>.</p>
+	 	<div>
+	</div><!-- link back to case study page -->
+
+<?php get_footer(); ?>
